08bed3411ddbcaab3ca02f704eda3304b354b06a
sheethalpaulson <sheethupaul@gmail.com>
2023-03-15 12:50:35 +0530

    env updated, cors url and other changes updated

---
+++ config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+console.log('process.env.DB_URI',process.env.DB_URI);
+
+const connectDatabase = () => {
+  mongoose
+    .connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+    .then((data) => {
+      console.log(`Mongodb connected with server: ${data.connection.host}`);
+    });
+};
+
+module.exports = connectDatabase;
