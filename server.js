4273b92430647ae09e97b4d9fbd6f155a180d1df
sheethalpaulson <sheethupaul@gmail.com>
2023-03-15 17:56:47 +0530

    access control allow origin fix

--- server.js
+++ server.js
@@ -36,18 +36,8 @@ process.on("unhandledRejection", (err) => {
 app.use(
   cors(
     {
-      origin: ["http://localhost:3000", "https://sheethal-ecommerce-site.onrender.com"],
-      methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
-      "headers.allow": ["Authorization", "Accept", "Content-Type", "Access-Control-Request-Headers", "Access-Control-Request-Method"],
-        "credentials": true,
-        "cache": 86400
+      "origin": ["http://localhost:3000", "https://sheethal-ecommerce-site.onrender.com"],
+      "methods": ['GET','POST','DELETE','UPDATE','PUT','PATCH']
     }
   )
 );
-
-// app.options('*', cors());
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
-// })
