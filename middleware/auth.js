b89996ec01c1f485c1bdfa1e99b64ec50183a43d
sheethalpaulson <sheethupaul@gmail.com>
2023-03-28 17:35:57 +0530

    cookie issue

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
-  const token = req.cookies?.token ?? req.headers.authorization.split(' ')[1];
+  const token = req.cookies?.token ?? req.headers.authorization?.split(' ')[1];
 
   if (!token) {
     return next(new ErrorHander("Please Login to access this resource", 401));
